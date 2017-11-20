4bf0fb523055365d6d1caef86947c2c698fa805e
Artem Grachov <grachov2195@gmail.com>
2017-11-20 13:00:40 +0200

    HTML/CSS Immidiately help page. Modal window size

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ import '../pug/pages/index.pug';
 import '../pug/pages/style.pug';
 import '../pug/pages/projects.pug';
 import '../pug/pages/archive.pug';
+import '../pug/pages/immidiately.pug';
 import 'normalize.css';
 import '../scss/main.scss';
 

f2f45643acc5075b8b93a9709630a4255cc05fb7
Artem Grachov <grachov2195@gmail.com>
2017-11-21 14:31:36 +0200

    CSS / JS Modal window open/close

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,3 +1,31 @@
-const openModal = function (id) {
+const modalAnimDur = 500;
 
+const openModal = function (event, modalId) {
+    event.preventDefault();
+    const modal = $('#' + modalId);
+    modal.addClass('active');
+    modal.find('.modal-wrap').css({
+        'animation-name': 'modalOpen',
+        'animation-duration': modalAnimDur + 'ms'
+    })
+    $('.wrapper').addClass('wrapper_blur');
+}
+
+const closeModal = function (event) {
+    event.preventDefault();
+    const modal = $(event.target.closest('.modal'));
+
+    modal.removeClass('active');
+    modal.css({
+        'display': 'block'
+    })
+    modal.find('.modal-wrap').css({
+        'animation-name': 'modalClose',
+        'animation-duration': modalAnimDur + 'ms'
+    })
+    setTimeout(() => {
+        modal.css({
+            display: ''
+        })
+    }, modalAnimDur - 50)
 }
--- gulp/path/js-libs.js
+++ gulp/path/js-libs.js
@@ -1,1 +1,1 @@
-module.exports = [];
+module.exports = ['node_modules/jquery/dist/jquery.min.js'];

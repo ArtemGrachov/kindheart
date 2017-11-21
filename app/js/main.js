de8952f61e29040bb5a4960eca150f90a1845030
Artem Grachov <grachov2195@gmail.com>
2017-11-21 16:09:20 +0200

    JS Toggle projects in projects lists

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     tabs.init();
+    toggleActive('.projects-item');
 })
+
+const toggleActive = function (selector) {
+    $(selector).on('click', function () {
+        const $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this
+                .addClass('active')
+                .siblings()
+                .removeClass('active');
+        }
+    })
+}
